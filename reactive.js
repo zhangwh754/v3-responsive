67373c75b1e9ee711bb1de27dd378e2a32974ada
张文皓 <zhangwh754@163.com>
2022-12-06 10:57:39 +0800

    ✨ feat: 响应式简易实现

---
+++ reactive.js
@@ -0,0 +1,19 @@
+import { track, trigger } from './effect.js'
+const isObject = target => target != null && typeof target == 'object'
+export const reactive = target => {
+  return new Proxy(target, {
+    get(target, key, receiver) {
+      const res = Reflect.get(target, key, receiver)
+      track(target, key)
+      if (isObject(res)) {
+        return reactive(res)
+      }
+      return res
+    },
+    set(target, key, value, receiver) {
+      const res = Reflect.set(target, key, value, receiver)
+      trigger(target, key)
+      return res
+    }
+  })
+}
---
+++ reactive.ts
@@ -0,0 +1,27 @@
+import { track, trigger } from './effect'
+
+const isObject = target => target != null && typeof target == 'object'
+
+export const reactive = <T extends object>(target: T) => {
+  return new Proxy(target, {
+    get(target, key, receiver) {
+      const res = Reflect.get(target, key, receiver) as object
+
+      track(target, key)
+
+      // 如果是对象，递归响应式
+      if (isObject(res)) {
+        return reactive(res)
+      }
+
+      return res
+    },
+    set(target, key, value, receiver) {
+      const res = Reflect.set(target, key, value, receiver)
+
+      trigger(target, key)
+
+      return res
+    }
+  })
+}
